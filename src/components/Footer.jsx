592ee6f1cc3ee77ed70b85d1afd2284935ca1611
anshif-8590 <anshifp949@gmail.com>
2025-12-25 15:23:12 +0530

    frontend work almost completd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="px-6 py-16 mt-10 lg:py-10 lg:mt-28 text-center">
 
-export default Footer
+      {/* Arabic Dua */}
+      <p
+        className="font-arabic text-lg text-primary mb-4"
+        style={{ direction: "rtl" }}
+      >
+        بَارَكَ ٱللَّٰهُ لَكُمَا وَبَارَكَ عَلَيْكُمَا وَجَمَعَ بَيْنَكُمَا فِي خَيْرٍ
+      </p>
+
+      {/* English Translation */}
+      <p className="text-body text-primary/70 max-w-xl mx-auto mb-8">
+        “May Allah bless you both, and bless your union, and bring you together
+        in goodness.”
+      </p>
+
+      {/* Divider */}
+      <div className="w-20 h-[1px] mx-auto  bg-[var(--color-border)]" />
+
+      {/* Copyright */}
+      <p className="text-xs tracking-widest text-primary/60 uppercase">
+        © 2026 Shafeeque & Afeefa Wedding
+      </p>
+
+    </footer>
+  );
+};
+
+export default Footer;
